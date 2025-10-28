cedfe80ccac0c8a0114509e5b12e99352e825624
JLL-Rudenko-Dmitriy <jll.rudenkodmitriy@gmail.com>
2025-10-29 01:01:56 +0300

    change config

--- lab-3/lab-3/vite.config.js
+++ lab-3/lab-3/vite.config.js
@@ -3,15 +3,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
     root: 'pages',
-    plugins: [
-        viteStaticCopy({
-          targets: [
-            { src: '../images/**/*', dest: 'images' },
-            { src: '../styles/**/*', dest: 'styles' },
-            { src: '../assets/**/*', dest: 'assets'}
-          ]
-        })
-    ],
+    publicDir: 'public',
     server: {open: '/homepage/' },
     build: {
         rollupOptions: {
